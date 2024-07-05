253fb0ed64352421141ce6dabfb1f7b38d907717
weimao <luanty@163.com>
2024-07-05 09:55:35 +0800

    update

---
+++ ports/stm32l4/boards/l433/board.h
@@ -0,0 +1,85 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Matthew McGowan for Blues Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef BOARD_H_
+#define BOARD_H_
+
+//--------------------------------------------------------------------+
+// Button
+//--------------------------------------------------------------------+
+
+//--------------------------------------------------------------------+
+// LED
+//--------------------------------------------------------------------+
+
+//--------------------------------------------------------------------+
+// Neopixel
+//--------------------------------------------------------------------+
+
+// Number of neopixels
+#define NEOPIXEL_NUMBER       0
+
+//--------------------------------------------------------------------+
+// Flash
+//--------------------------------------------------------------------+
+
+// Flash size of the board
+#define BOARD_FLASH_SIZE  (256 * 1024)
+
+//--------------------------------------------------------------------+
+// USB UF2
+//--------------------------------------------------------------------+
+
+#define USB_VID           0x484C
+#define USB_PID           0x0002
+#define USB_MANUFACTURER  "Helix Lab"
+#define USB_PRODUCT       "Elysium UF2 Boot"
+
+#define UF2_PRODUCT_NAME  USB_PRODUCT
+#define UF2_BOARD_ID      "STM32L4"
+#define UF2_VOLUME_LABEL  "ElysiumBOOT"
+#define UF2_INDEX_URL     "https://helix.site"
+
+//--------------------------------------------------------------------+
+// UART
+//--------------------------------------------------------------------+
+
+//#define UART_DEV              USART1
+#define UART_CLOCK_ENABLE     __HAL_RCC_USART1_CLK_ENABLE
+#define UART_CLOCK_DISABLE    __HAL_RCC_USART1_CLK_DISABLE
+#define UART_GPIO_PORT        GPIOA
+#define UART_GPIO_AF          GPIO_AF7_USART1
+#define UART_TX_PIN           GPIO_PIN_9
+#define UART_RX_PIN           GPIO_PIN_10
+
+#define USB_NO_VBUS_PIN
+#define USB_NO_USB_ID_PIN
+#define BOARD_STACK_APP_START (0x20000000U)
+#define BOARD_STACK_APP_END   (BOARD_STACK_APP_START+(640*1024))
+
+
+
+void clock_init(void);
+
+#endif
